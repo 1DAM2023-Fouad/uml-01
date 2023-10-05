d1c2922ee8644eb57f6f4ac4d5a9a49624df343b
Fouad0110101 <fouad.aha@educa.jcyl.es>
2023-10-05 16:16:20 +0200

    Inicio del proyecto

---
+++ src/Placabase.java
@@ -0,0 +1,22 @@
+public class Placabase {
+    //Estados, atributos, propiedades
+    public String marca;
+    public String modelo;
+    public Double precio;
+
+    public String getNombreCompleto() {
+        return marca + modelo + precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+}
---
+++ src/Precesador.java
@@ -0,0 +1,25 @@
+public class Precesador {
+    //Estados, atributos, propiedaes
+    public String marca;
+    public String modelo;
+    public Double precio;
+
+    //Comportamiento o método (funciones)
+    // public (+) método accesible desde cualquier clase.
+    // String: tipo devuleto por el método
+    public String getNombreCompleto(){
+        //Concatenas cadenas con +
+        // AMD 7600 32.2
+        return marca + " " + modelo + " " + precio;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){ return modelo; }
+
+    public Double getPrecio(){
+        return precio;
+    }
+}
